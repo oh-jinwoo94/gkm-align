16ecc4b505989983ee986852ab0c0e7571ec2d87
Jin Woo Oh <64922399+oh-jinwoo94@users.noreply.github.com>
2023-10-16 22:03:08 -0400

    Update run_gkmalign.sh
    Former-commit-id: 45872b9140cccdf561dc198a34eede7df75c8038

--- example/whole_genome/run_gkmalign.sh
+++ example/whole_genome/run_gkmalign.sh
@@ -5,6 +5,7 @@
 echo "../../data/mouse_genomic_background_model_p_0.1.out" > masker_models.txt
 echo "../../data/human_genomic_background_model_p_0.1.out" >> masker_models.txt
 
+mkdir output_files
 ../../bin/gkm_align  -t 1  human_mouse_WG_syntenic_intergenic_loci.to_align -d ../../data/genomes/ -g masker_models.txt   -p 50 -o output_files -n human_mouse_WG_syntenic_intergenic_loci
 #../../bin/gkm_align  -t 2  HBB_LCR_enhancers_mm10.bed  -c output_files/HBB_LCR_mm10-hg38.coord -q mm10 -m -o output_files -n HBB_LCR_enhancers_mm10_mapped_to_hg38
 
