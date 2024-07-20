5f69e423081d1fa1162bd27722d5bc139a66e8a9
Jin Woo Oh <joh27@jhu.edu>
2024-07-20 02:23:26 -0400

    test

--- clear_and_push.sh
+++ clear_and_push.sh
@@ -1,29 +1,3 @@
 #!/bin/sh
 
-## for development only ##
-
-# remove binary files
-for file in $(ls bin/|grep -v .py)
-do
-	rm bin/${file}
-done
-cd src; make clean
-
-
-cd ..; rm -r data
-
-
-# clear example data
-# HBB
-rm example/HBB_LCR/masker_models.txt; rm -r example/HBB_LCR/output_files
-
-# FADS
-rm example/FADS_cluster/masker_models.txt;  rm -r example/FADS_cluster/output_files
-
-# WG
-for file in $(ls example/whole_genome|grep -v .sh)
-do 
-	rm -r example/whole_genome/$file
-done
-
 git add .; git commit -m "test"; git push gkm main
