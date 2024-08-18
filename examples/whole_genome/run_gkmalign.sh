49614d5f782c9c869892649be658538b5ba4d806
Jin Woo Oh <joh27@jhu.edu>
2024-08-18 03:39:09 -0400

    readme_update

--- examples/whole_genome/run_gkmalign.sh
+++ examples/whole_genome/run_gkmalign.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+to_align=$1 # e.g., human_mouse_WG_syntenic_intergenic_loci.to_align
 
 echo "../../data/mouse_genomic_background_model_p_0.1.out" > masker_models.txt
 echo "../../data/human_genomic_background_model_p_0.1.out" >> masker_models.txt
 
-../../bin/gkm_align  -t 1  human_mouse_WG_syntenic_intergenic_loci.to_align -d ../../data/genomes/ -g masker_models.txt   -p 50 -o output_files -n human_mouse_WG_syntenic_intergenic_loci -G
+../../bin/gkm_align  -t 1  ${to_align} -d ../../data/genomes/ -g masker_models.txt   -p 50 -o output_files -n human_mouse_WG_syntenic_intergenic_loci -G
 
 
 
