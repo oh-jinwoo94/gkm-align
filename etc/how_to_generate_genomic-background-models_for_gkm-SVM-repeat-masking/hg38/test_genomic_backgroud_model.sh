23771331e9d34cc43b000d558c2add28aea479f8
Jin Woo Oh <joh27@jhu.edu>
2024-12-13 03:04:22 -0500

    instruction_for_generating_gkm-SVM_background_models
    Former-commit-id: cb4eedbfb7dafeeb1c95b3a399c58da36b5fcd00

---
+++ etc/how_to_generate_genomic-background-models_for_gkm-SVM-repeat-masking/hg38/test_genomic_backgroud_model.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+# Testing genomic background models with gkm-align by masking fasta files.
+
+
+# Mask human sample sequences using the human background model (downloaded via setup.py).
+../../../bin/mask_fa test_hg38.fa ../../../data/human_genomic_background_model_p_0.1.out test_hg38_gkm-masked_h.fa
+
+# Mask human sample sequences using the mouse background model.
+../../../bin/mask_fa test_hg38.fa ../../../data/mouse_genomic_background_model_p_0.1.out test_hg38_gkm-masked_m.fa
+
+
+# Comparing the similarity between human and mouse background models.
+# gkm-SVM background model identifies overrepresented simple sequence patterns in genomic sequences,
+# and most of these patterns are species-invariant. So, if generating these background models is difficult for you,
+# you can also consider using the background models we have uploaded (e.g., human-mouse average model for masking another species).
+
+python compare_fa.py  # output: 99.79% match
+
